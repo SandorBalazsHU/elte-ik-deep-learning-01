ad8862dbe1f64543a5235f7aa66de3d55f793f02
Sándor Balázs <sabuaai@INF.ELTE.HU>
2023-10-23 20:30:29 +0200

    Commenting

---
+++ env.bat
@@ -0,0 +1,22 @@
+cd C:\Users\sandorbalazs\OneDrive - Eotvos Lorand Tudomanyegyetem\MSC ERA\MSC\03\03_Deep Learning Gy\elte-ik-deep-learning-01
+python -m venv venv
+venv\Scripts\activate
+pip install numpy
+pip install torch==2.1.0+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torchvision
+pip install torchsummary
+pip install pandas
+pip install jupyter
+pip install graphviz
+pip install matplotlib
+pip install IPython
+pip install sklearn
+pip install seaborn
+pip install pretty_confusion_matrix
+
+jupyter notebook
+
+time
+copy
+os
+warnings
